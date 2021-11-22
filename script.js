a0980baf036ef876175584910b00a210f233f548
kpuk4o <kpuk4o@gmail.com>
2021-11-22 00:47:59 -0600

    before the bonus mission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener('load',function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json()
+    }).then(function(json){
+        console.log(json)
+        let container = document.getElementById("container")
+        let astronauts = ''
+        for  (let astronaut of json){
+            astronauts += `<div class="astronaut">
+                <div class = "bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space:${astronaut.hoursInSpace}</li>
+                        <li>Active:${astronaut.active}</li>
+                        <li>Skills:${astronaut.skills.join(", ")}</li>
+                    </ul>
+                </div>
+                <img src="${astronaut.picture}" alt="" class="avatar" />
+            </div>`
+        }
+        container.innerHTML = astronauts;
+    })
+})
