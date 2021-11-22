9e1279b1ff54825d9fcb948e317ae92e67f910c4
kpuk4o <kpuk4o@gmail.com>
2021-11-22 01:14:32 -0600

    with the bonus mission

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ window.addEventListener('load',function(){
     }).then(function(json){
         console.log(json)
         let container = document.getElementById("container")
+        let count = document.getElementById('count')
+        count.innerHTML += `${json.length}`
         let astronauts = ''
+        json.sort(function(a,b){
+            return (a.hoursInSpace < b.hoursInSpace) ? 1 : -1
+        })
         for  (let astronaut of json){
             astronauts += `<div class="astronaut">
                 <div class = "bio">
                     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                     <ul>
                         <li>Hours in space:${astronaut.hoursInSpace}</li>
-                        <li>Active:${astronaut.active}</li>
+                        <li style="color:${astronaut.active ? 'green' : 'red'}">Active:${astronaut.active}</li>
                         <li>Skills:${astronaut.skills.join(", ")}</li>
                     </ul>
                 </div>
                 <img src="${astronaut.picture}" alt="" class="avatar" />
             </div>`
         }
-        container.innerHTML = astronauts;
+        container.innerHTML = astronauts + `<p>Astronaut Count: ${json.length} </p>`;
     })
 })
